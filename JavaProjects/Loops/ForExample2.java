4326f5c77d093c1b7506dcbf6caac60377dad609
Andreasoteloes <143759043+Andreasoteloes@users.noreply.github.com>
2023-09-05 16:36:19 -0500

    Add files via upload

---
+++ JavaProjects/Loops/ForExample2.java
@@ -0,0 +1,8 @@
+public class ForExample2 {  
+public static void main(String[] args) {  
+    //Using no condition in for loop  
+    for(;;){  
+        System.out.println("infinitive loop");  
+    }  
+}  
+}  
