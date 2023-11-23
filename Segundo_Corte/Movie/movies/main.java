3b248b69f004388c55348adf5c6039c143015d0c
Andreasoteloes <143759043+Andreasoteloes@users.noreply.github.com>
2023-11-23 18:08:57 -0500

    Add files via upload

---
+++ Segundo_Corte/Movie/movies/main.java
@@ -0,0 +1,10 @@
+package movies;
+
+import javax.swing.JOptionPane;
+import movie.ui.UIPeliculas;
+
+public class main {
+    public static void main(String[] args){
+        UIPeliculas.interfazUsuario();
+    }
+}
